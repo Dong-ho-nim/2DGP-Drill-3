84c33a21fe56bdf5cb2cec6bc6001154bbaf7cd5
Dong-ho-nim <shindh758@gmail.com>
2025-09-16 11:16:57 +0900

    사각운동기본뼈대완성

--- Character_move.py
+++ Character_move.py
@@ -5,14 +5,40 @@ open_canvas()
 # fill here
 boy = load_image('character.png')
 
+
+def move_top():
+    pass
+
+
+def move_right():
+    pass
+
+
+def move_left():
+    pass
+
+
+def move_bottom():
+    pass
+
+
 def move_rectangle():
     print("Move rectangle")
+    move_top()
+    move_right()
+    move_bottom()
+    move_left()
     pass
 
 
 def move_circle():
+    print("Move circle")
+    r = 200
+    for deg in range(0, 360):
+        x = r * math.cos(math.radians(deg)) + 400
+        y = r * math.sin(math.radians(deg)) + 300
     clear_canvas_now()
-    boy.draw_now(400,300)
+    boy.draw_now(x,y)
     delay(0.1)
     pass
 
@@ -20,6 +46,7 @@ def move_circle():
 while True:
     move_circle()
     move_rectangle()
+    break
     pass
 
 close_canvas()
